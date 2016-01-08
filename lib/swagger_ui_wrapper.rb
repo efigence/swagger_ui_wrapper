c4ffbef42a148e3061a6f6ba537c9c7c1f395852
lizhe <lnz013@qq.com>
2016-01-08 12:21:52 +0800

    very basic implementation

--- lib/swagger_ui_wrapper.rb
+++ lib/swagger_ui_wrapper.rb
@@ -2,5 +2,4 @@ require 'swagger_ui_wrapper/version'
 require 'swagger_ui_wrapper/rack/middleware'
 
 module SwaggerUiWrapper
-  # Your code goes here...
 end
--- lib/swagger_ui_wrapper/rack/middleware.rb
+++ lib/swagger_ui_wrapper/rack/middleware.rb
@@ -4,14 +4,21 @@ require "rack/utils"
 module SwaggerUiWrapper
   module Rack
     class MiddleWare
-      def initialize(app, path = '/swagger')
+      def initialize(app, options)
         @app = app
-        @path = path
+        @api_url_base = options[:api_url_base]
+        @path = options[:path] || '/swagger/'
+        swagger_path = File.expand_path('../../../vendor/assets', File.dirname(__FILE__))
+        @swagger_assets_handler = ::Rack::Static.new @app, root: swagger_path,
+                                                           urls: ['/swagger'],
+                                                           index: 'index.html'
       end
 
       def call(env)
         if env['REQUEST_URI'] == @path
-          [200, {'Content-Type' => 'text/html'}, ['SwaggerUiWrapper']]
+          [301, {'Location' => env['REQUEST_URI'] + '?url=' + @api_url_base, 'Content-Type' => 'text/html', 'Content-Length' => '0'}, []]
+        elsif env['REQUEST_URI'].start_with? @path
+          @swagger_assets_handler.call(env)
         else
           @app.call(env)
         end
