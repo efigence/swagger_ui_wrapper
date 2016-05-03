e2d61c5a370566af4efbfc3a8d898e8c94739754
lizhe <lnz013@qq.com>
2016-05-03 17:40:02 +0800

    redirect loop bugfix

--- lib/swagger_ui_wrapper/rack/middleware.rb
+++ lib/swagger_ui_wrapper/rack/middleware.rb
@@ -15,7 +15,7 @@ module SwaggerUiWrapper
       end
 
       def call(env)
-        if env['REQUEST_PATH'] == @path
+        if env['REQUEST_URI'].end_with? @path
           [301, {'Location' => env['REQUEST_URI'] + '?url=' + @api_url_base, 'Content-Type' => 'text/html', 'Content-Length' => '0'}, []]
         elsif env['REQUEST_URI'].start_with? @path
           @swagger_assets_handler.call(env)
