221811167b3acd51ac718b5957d5336fe32837d2
jasiekkk <jkamyk@efigence.com>
2016-06-30 08:32:16 +0200

    added redirect

--- lib/swagger_ui_wrapper/rack/middleware.rb
+++ lib/swagger_ui_wrapper/rack/middleware.rb
@@ -9,9 +9,10 @@ module SwaggerUiWrapper
         @api_url_base = options[:api_url_base] # http://test-server.com
         @path = options[:path] || '/swagger/'
         swagger_path = File.expand_path('../../../vendor/assets', File.dirname(__FILE__))
-        @swagger_assets_handler = ::Rack::Static.new @app, root: swagger_path,
-                                                          urls: ['/swagger'] #,
-                                                          # index: 'v1.html' # switched to redirect instead of render
+        @swagger_assets_handler = ::Rack::Static.new(@app, root: swagger_path,
+                                                           urls: ['/swagger'])
+        @swagger_assets_handler_v1 = ::Rack::Static.new(@app, root: swagger_path,
+                                                              urls: {"/swagger/v1" => "/swagger/v1.html"})
       end
 
       def call(env)
@@ -20,12 +21,13 @@ module SwaggerUiWrapper
         if env_request_path && env_request_path.start_with?(@path)
           if env_request_path == '/swagger/'
             [302, {'Content-Type' => 'text',
-                   'Location' => (@api_url_base || (env['rack.url_scheme'] + "://" + env["HTTP_HOST"].to_s)) + @path + 'v1.html',
+                   'Location' => (@api_url_base || (env['rack.url_scheme'] + "://" + env["HTTP_HOST"].to_s)) + @path + 'v1',
                    'Content-Type' => 'text/html; charset=UTF-8'}, ['302 found']]
           else
             # some hack for GRAPE:
             # _env = env.merge("QUERY_STRING" => "url=http://localhost:9292/v1/swagger_doc")
             # @swagger_assets_handler.call(_env)
+            @swagger_assets_handler_v1.call(env)
             @swagger_assets_handler.call(env)
           end
         else
